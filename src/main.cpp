eb5f3b6aa101016acd81782fab90c20a3cf71f6f
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-12-05 18:09:00 +0300

    working on main menu (console interface)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
+int option;
+bool inputEntered = false;
+
+void showMainMenu();
 
 int main() {
-    std::cout << "Hello World!\n";
+    showMainMenu();
+}
+
+void showMainMenu() {
+    while (!inputEntered) {
+        std::cout << "-------------------------------------------------\n";
+        std::cout << "1. New order\n";
+        std::cout << "2. Log in as administrator\n";
+        std::cout << "3. Exit\n";
+        std::cout << "-------------------------------------------------\n";
+
+        std::cout << "Input: ";
+        std::cin >> option;
+
+        inputEntered = option >= 1 && option <= 3;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cout << "Your input is invalid. (you only can use integer values for input)\n";
+        }
+        else if (!inputEntered)
+            std::cout << "Your input is invalid. (input must be either 1, 2 or 3)\n";
+    }
 }
