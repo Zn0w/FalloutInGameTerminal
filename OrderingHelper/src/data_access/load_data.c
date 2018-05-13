c591aa4441e1675fa82a4bd3fd16c15f1d13cb3c
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-05-13 20:22:24 +0300

    clean install

--- OrderingHelper/src/data_access/load_data.c
+++
@@ -1,8 +0,0 @@
-#include "load_data.h"
-
-Item* get_items()
-{
-	static Item* items; // Static in order for function to actually return an array ic C
-
-	return items;
-}
--- OrderingHelper/src/data_access/load_data.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOAD_DATA_H
-#define LOAD_DATA_H
-
-#include "../domain/item.h"
-
-Item* get_items();
-
-#endif
