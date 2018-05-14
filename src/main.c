7c65529083fc0cd0f999f2b6d79481b369d645eb
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-05-14 20:46:26 +0300

    added a welcome screen

--- src/main.c
+++ src/main.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-	printf("Hello World!\n");
+	printf("Welcome to the ZOS!\n");
+	printf("***\n");
+	printf("ZOS - Znow Operating System is an alternative to the RobCo Industries' Unified Operating System (UOS)\n");
+	printf("***\n");
+	printf("ZOS is officially intended to work on a hardware supplied by RobCo Industries, namely:\n");
+	printf("RobCo NX-12 terminal, RobCo E-330 terminal, RobCo E-601 terminal, RobCo RX-6550 terminal, RobCo RX-9000 terminal\n");
+	printf("***\n");
+
+	printf("Press enter in order to proceed to the startup menu.");
+
+	getchar();
 	
 	return 0;
 }
