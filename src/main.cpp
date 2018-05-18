39206533fa5b4bc925ee0a5aecc6bf77c7d2c915
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-05-18 19:55:32 +0300

    added the welcome screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,61 @@
 
 int current_page = 0;
 
+// current_page = 0 is terminal's welcome screen
+// current_page = 1 is terminal's initial screen
+// current_page > 1 is the rest
+
+bool running = true;
+
+void close_terminal();
+
+void display_welcomeScreen();
+
 int main()
 {
-	/*printf("Welcome to the ZOS!\n");
-	printf("***\n");
-	printf("ZOS - Znow Operating System is an alternative to the RobCo Industries' Unified Operating System (UOS)\n");
-	printf("***\n");
-	printf("ZOS is officially intended to work on a hardware supplied by RobCo Industries, namely:\n");
-	printf("RobCo NX-12 terminal, RobCo E-330 terminal, RobCo E-601 terminal, RobCo RX-6550 terminal, RobCo RX-9000 terminal\n");
-	printf("***\n");
-
-	printf("Press enter in order to proceed to the startup menu.");
-
-	getchar();*/
-
-	while (true)
+	while (running)
 	{
 		// clear the console screen
 		system("cls");
 
-		std::cout << "Test" << std::endl;
+		if (current_page == 0)
+		{
+			display_welcomeScreen();
+		}
 
 		// Pauses the execution until any key is pressed (">nul" means not to display 'Press any key to continue')
 		system("pause>nul");
+
+		// Process pressed key
+
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			running = false;
+		}
+		else if (GetAsyncKeyState(VK_RETURN))
+		{
+			//if (current_page == )
+		}
 	}
+
+	close_terminal();
 	
 	return 0;
+}
+
+void close_terminal()
+{
+	std::cout << "Terminal has been closed." << std::endl;
+}
+
+void display_welcomeScreen()
+{
+	std::cout << "Welcome to the ZOS!" << std::endl;
+	std::cout << "***" << std::endl;
+	std::cout << "ZOS - Znow Operating System is an alternative to the RobCo Industries' Unified Operating System (UOS)" << std::endl;
+	std::cout << "***" << std::endl;
+	std::cout << "ZOS is officially intended to work on a hardware supplied by RobCo Industries, namely:" << std::endl;
+	std::cout << "RobCo NX-12 terminal, RobCo E-330 terminal, RobCo E-601 terminal, RobCo RX-6550 terminal, RobCo RX-9000 terminal" << std::endl;
+	std::cout << "***" << std::endl;
+	std::cout << "Press enter in order to proceed to the startup menu." << std::endl;
 }
